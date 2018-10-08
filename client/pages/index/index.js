8a54c1ff778a8b989a28359e4fb46a2ec612124c
Yicoding <804413572@qq.com>
2018-10-08 16:54:16 +0800

    获取用户信息bug

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -49,7 +49,9 @@ Page({
     isShow: false
   },
   onLoad: function () {
+    console.log('index: onLoad')
     if (app.globalData.userInfo) {
+      console.log(app.globalData.userInfo, 'index：存在全局用户信息')
       this.setData({
         userInfo: app.globalData.userInfo
       },() => {
@@ -58,10 +60,12 @@ Page({
         })
       })
     } else if (this.data.canIUse){
+      console.log('执行this.data.canIUse')
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         console.log(res.userInfo, 'app.userInfoReadyCallback')
+        app.globalData.userInfo = res.userInfo
         this.setData({
           userInfo: res.userInfo
         },() => {
@@ -71,6 +75,7 @@ Page({
         })
       }
     } else {
+      console.log('index: 最后情况')
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
