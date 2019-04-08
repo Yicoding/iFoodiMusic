69d5131e353e7739d57ed19904fb153b40f731a1
Yicoding <804413572@qq.com>
2019-04-08 10:02:07 +0800

    add

--- server/config.js
+++ server/config.js
@@ -37,7 +37,9 @@ const CONF = {
         // 文件夹
         uploadFolder: 'present'
     },
-
+    qcloudAppId: '1257454171',
+    qcloudSecretId: 'AKIDmtlOmudVu31jBMs4XkiNnzvd6KI0Fopa',
+    qcloudSecretKey: 'e2B4h7GAoCLEqDPL2TegSyEJ8YkhQu9A',
     // 微信登录态有效期
     wxLoginExpires: 7200,
     wxMessageToken: 'abcdefgh'
